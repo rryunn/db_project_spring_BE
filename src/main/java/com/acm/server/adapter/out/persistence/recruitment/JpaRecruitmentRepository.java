9241b169423159f831b2d75de314be8ea663d853
Hyeongmin Choi <jhm7916@ajou.ac.kr>
2025-11-15 23:20:03 +0900

    Dto 추가 및 N+1 해결

--- src/main/java/com/acm/server/adapter/out/persistence/recruitment/JpaRecruitmentRepository.java
+++ src/main/java/com/acm/server/adapter/out/persistence/recruitment/JpaRecruitmentRepository.java
@@ -4,6 +4,7 @@ import com.acm.server.adapter.out.entity.RecruitmentEntity;
 import com.acm.server.application.recruitment.dto.RecruitmentCounterProjection;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,10 +15,18 @@ public interface JpaRecruitmentRepository extends JpaRepository<RecruitmentEntit
     //findById도 이미 제공됨
     //deleteById도 제공해줌.
     //List<RecruitmentEntity> findByEndDateAfterOrderByEndDateAsc(LocalDate now, Pageable pageable);
+
+    @EntityGraph(attributePaths = "club")  // ✅ club 같이 가져오게
+    @Query("select r from RecruitmentEntity r")
+    List<RecruitmentEntity> findAllWithClub();
+
+    @EntityGraph(attributePaths = "club")
     List<RecruitmentEntity> findByClub_Id(Long clubId);
     @Query("SELECT r FROM RecruitmentEntity r " +
             "WHERE r.type = '상시모집' OR " +
             "(r.type = '수시모집' AND r.endDate >= CURRENT_DATE)")
+
+    @EntityGraph(attributePaths = "club")
     List<RecruitmentEntity> findMainRecruitments();
 
     @Query("""
